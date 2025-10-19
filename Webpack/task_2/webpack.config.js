64ac0c2cdf9da695499462fcc271233718a8336f
laetitiabeles <laetitiabeles9@gmail.com>
2025-10-19 15:03:33 +0200

    task_2

---
+++ Webpack/task_2/webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './js/dashboard_main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name][ext]',
+        },
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: { progressive: true, quality: 75 },
+              optipng: { enabled: true },
+              pngquant: { quality: [0.7, 0.9], speed: 3 },
+              gifsicle: { interlaced: false },
+            },
+          },
+        ],
+        enforce: 'pre',
+      },
+    ],
+  },
+  performance: {
+    hints: false,
+  },
+};
