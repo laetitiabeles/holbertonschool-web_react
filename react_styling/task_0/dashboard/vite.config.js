844fa0a8b5bcf3e1a5af099ee5de951ca1ed93c9
laetitiabeles <laetitiabeles9@gmail.com>
2025-12-05 16:45:11 +0100

    feat: Update dependencies and remove unused TailwindCSS plugin

--- react_styling/task_0/dashboard/vite.config.js
+++ react_styling/task_0/dashboard/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwind from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwind()],
+  plugins: [react()],
   base: '/holbertonschool-web_react/'
 })
