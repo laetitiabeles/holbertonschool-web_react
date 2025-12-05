e3e01142ed028e1016ecb09688d6cd32329f1ec2
laetitiabeles <laetitiabeles9@gmail.com>
2025-12-05 16:48:52 +0100

    feat: Add TailwindCSS Vite plugin to enhance styling capabilities

--- react_styling/task_0/dashboard/vite.config.js
+++ react_styling/task_0/dashboard/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwind from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwind()],
   base: '/holbertonschool-web_react/'
 })
